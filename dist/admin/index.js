9b0fc4a4019fd6c627995ffb72115e411b330075
cablej <jackhcable@gmail.com>
2016-05-31 11:09:27 -0500

    Added JSCS and complied with code style

--- dist/admin/index.js
+++ dist/admin/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.router = undefined;
 exports.setUser = setUser;
@@ -37,34 +37,34 @@ var auth;
 var router = exports.router = _express2.default.Router();
 
 var attachClass = function attachClass() {
-	return (0, _composableMiddleware2.default)().use(function (req, res, next) {
-		// TODO: Check that the classname exists (in mongoose.models)
-		req.class = _mongoose2.default.model(req.params.className);
-		next();
-	});
+  return (0, _composableMiddleware2.default)().use(function (req, res, next) {
+    // TODO: Check that the classname exists (in mongoose.models)
+    req.class = _mongoose2.default.model(req.params.className);
+    next();
+  });
 };
 
 function setUser(_user) {
-	User = _user;
+  User = _user;
 
-	_snapmobileAuthserver2.default.setUser(User);
-	auth = _snapmobileAuthserver2.default.authService;
+  _snapmobileAuthserver2.default.setUser(User);
+  auth = _snapmobileAuthserver2.default.authService;
 
-	/**
-  * Admin routes for CMS
-  */
-	router.get('/:className/schema', auth.hasRole('admin'), attachClass(), controller.getSchema);
-	router.post('/:className/deleteMultiple', auth.hasRole('admin'), attachClass(), controller.destroyMultiple);
-	router.get('/:className/exportToCsv', auth.hasRole('admin'), attachClass(), controller.exportToCsv);
+  /**
+   * Admin routes for CMS
+   */
+  router.get('/:className/schema', auth.hasRole('admin'), attachClass(), controller.getSchema);
+  router.post('/:className/deleteMultiple', auth.hasRole('admin'), attachClass(), controller.destroyMultiple);
+  router.get('/:className/exportToCsv', auth.hasRole('admin'), attachClass(), controller.exportToCsv);
 
-	router.get('/:className/', auth.hasRole('admin'), attachClass(), controller.index);
-	router.get('/:className/:id', auth.hasRole('admin'), attachClass(), controller.show);
-	router.post('/:className/', auth.hasRole('admin'), attachClass(), controller.create);
-	router.put('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
-	router.patch('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
-	router.delete('/:className/:id', auth.hasRole('admin'), attachClass(), controller.destroy);
+  router.get('/:className/', auth.hasRole('admin'), attachClass(), controller.index);
+  router.get('/:className/:id', auth.hasRole('admin'), attachClass(), controller.show);
+  router.post('/:className/', auth.hasRole('admin'), attachClass(), controller.create);
+  router.put('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
+  router.patch('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
+  router.delete('/:className/:id', auth.hasRole('admin'), attachClass(), controller.destroy);
 }
 
 function setUtils(_utils) {
-	controller.setUtils(_utils);
+  controller.setUtils(_utils);
 }
--- src/admin/index.js
+++ src/admin/index.js
@@ -21,26 +21,28 @@ const attachClass = function() {
 };
 
 export function setUser(_user) {
-	User = _user;
+  User = _user;
 
-	authServer.setUser(User);
-	auth = authServer.authService;
+  authServer.setUser(User);
+  auth = authServer.authService;
 
-	/**
-	 * Admin routes for CMS
-	 */
-	router.get('/:className/schema', auth.hasRole('admin'), attachClass(), controller.getSchema);
-	router.post('/:className/deleteMultiple', auth.hasRole('admin'), attachClass(), controller.destroyMultiple);
-	router.get('/:className/exportToCsv', auth.hasRole('admin'), attachClass(), controller.exportToCsv);
+  /**
+   * Admin routes for CMS
+   */
+  router.get('/:className/schema', auth.hasRole('admin'), attachClass(), controller.getSchema);
+  router.post('/:className/deleteMultiple', auth.hasRole('admin'),
+    attachClass(), controller.destroyMultiple);
+  router.get('/:className/exportToCsv', auth.hasRole('admin'),
+    attachClass(), controller.exportToCsv);
 
-	router.get('/:className/', auth.hasRole('admin'), attachClass(), controller.index);
-	router.get('/:className/:id', auth.hasRole('admin'), attachClass(), controller.show);
-	router.post('/:className/', auth.hasRole('admin'), attachClass(), controller.create);
-	router.put('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
-	router.patch('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
-	router.delete('/:className/:id', auth.hasRole('admin'), attachClass(), controller.destroy);
+  router.get('/:className/', auth.hasRole('admin'), attachClass(), controller.index);
+  router.get('/:className/:id', auth.hasRole('admin'), attachClass(), controller.show);
+  router.post('/:className/', auth.hasRole('admin'), attachClass(), controller.create);
+  router.put('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
+  router.patch('/:className/:id', auth.hasRole('admin'), attachClass(), controller.update);
+  router.delete('/:className/:id', auth.hasRole('admin'), attachClass(), controller.destroy);
 }
 
 export function setUtils(_utils) {
-	controller.setUtils(_utils);
+  controller.setUtils(_utils);
 }
